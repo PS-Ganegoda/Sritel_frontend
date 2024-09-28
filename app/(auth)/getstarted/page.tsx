497b2d82c517d66112db1e52729c44a11e21f466
Nethmi Liyanage <nethmi2001liyanage@gmail.com>
2024-09-28 22:59:26 +0530

    fixed

--- app/(auth)/getstarted/page.tsx
+++ app/(auth)/getstarted/page.tsx
@@ -13,7 +13,7 @@ export default function GetStarted() {
 
       <div className="flex flex-col-reverse lg:flex-row w-full max-w-7xl mx-auto my-10 p-8 lg:p-16 bg-white dark:bg-gray-800 shadow-lg rounded-lg flex-grow">
         <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
-          <h1 className="text-3xl lg:text-5xl font-extrabold mb-4 text-[#c96cc3] dark:text-white">
+          <h1 className="text-3xl lg:text-5xl font-extrabold mb-4 text-[#c96cc3] dark:[#c96cc3]">
             Sri Tel Ltd (STL)
           </h1>
           <p className="text-lg mb-8 text-gray-600 dark:text-gray-300">
@@ -29,7 +29,7 @@ export default function GetStarted() {
 
             <Button
               type="button"
-              className="py-3 px-6 bg-transparent border border-[#2b4b55] text-[#2b4b55] font-semibold rounded-lg hover:bg-[#e5a8df] hover:border-[#2b4b55] dark:border-white dark:text-white dark:hover:bg-gray-700"
+              className="py-3 px-6 bg-transparent border border-[#0b4b55] text-[#0b4b55] font-semibold rounded-lg hover:bg-[#e5a8df] hover:border-[#2b4b55] dark:border-white dark:text-white dark:hover:bg-gray-700"
             >
               Learn More
             </Button>
